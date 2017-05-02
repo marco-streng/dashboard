1ca0df549c69d8bcab6dfb11a6f7f7d9dfb8d781
Marco Streng <marcostreng@me.com>
2017-05-02 20:20:41 +0200

    Added OpenPullRequest widget

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Dashboard from '../components/dashboard'
 import DateTime from '../components/widgets/datetime'
 import PageSpeedScore from '../components/widgets/psi'
 import JiraIssueCount from '../components/widgets/jira-issue-count'
+import BitbucketPullrequestsCount from '../components/widgets/bitbucket-pullrequest-count'
 
 // Theme
 import lightTheme from '../styles/light-theme'
@@ -15,8 +16,12 @@ export default () => (
 
     <PageSpeedScore url='https://github.com/' />
 
+    <BitbucketPullrequestsCount
+      url='https://api.bitbucket.org/'
+    />
+
     <JiraIssueCount
-      title='JIRA Open Bugs'
+      title='JIRA Issue Count'
       url='https://crossorigin.me/https://jira.atlassian.com/'
       query='type=Bug AND project="Bitbucket Server" AND resolution=Unresolved ORDER BY priority DESC,created DESC'
     />
